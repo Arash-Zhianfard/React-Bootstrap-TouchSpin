5d3790f135b4f38f8c84e93d2a613fd2f9f5bca5
Arash <arash.zhianfard@gmail.com>
2022-02-27 18:54:37 +0100

    intial configs for tests

---
+++ src/__test__/TouchSpin.test.tsx
@@ -0,0 +1,9 @@
+// import '@testing-library/jest-dom';
+// import { render, screen } from '@testing-library/react';
+// import TouchSpin from '../TouchSpin';
+describe('true is truthy and false is falsy', () => {
+  test('true is truthy', () => {
+    expect(true).toBe(true);
+  });
+});
+
