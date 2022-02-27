da3b31e430d33220fb7d11136c7e1050d1557c61
Arash <arash.zhianfard@gmail.com>
2022-02-27 19:30:45 +0100

    init test added

--- src/__test__/TouchSpin.test.tsx
+++ src/__test__/TouchSpin.test.tsx
@@ -1,7 +1,7 @@
 // import '@testing-library/jest-dom';
 // import { render, screen } from '@testing-library/react';
 // import TouchSpin from '../TouchSpin';
-describe('true is truthy and false is falsy', () => {
+describe('init test', () => {
   test('true is truthy', () => {
     expect(true).toBe(true);
   });
