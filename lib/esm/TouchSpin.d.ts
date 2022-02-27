7b8249a55c8c5167c3445a9e97b64322de1b1b2d
Arash <arash.zhianfard@gmail.com>
2022-02-27 19:30:11 +0100

    lib removed+new verison published

--- lib/esm/TouchSpin.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { TouchSpinProps } from './Type';
-declare const TouchSpin: ({ signAlignment, sign, initValue, step, min, max, size, decimals, counterHandler }: TouchSpinProps) => JSX.Element;
-export default TouchSpin;
--- lib/esm/index.d.ts
+++
@@ -1,3 +0,0 @@
-import "bootstrap/dist/css/bootstrap.css";
-export * from './TouchSpin';
-export { TouchSpinProps, BsSize } from './Type';
